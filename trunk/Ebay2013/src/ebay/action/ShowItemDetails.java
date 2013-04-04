e674f8724f0d3215ca9a15fb60cd9010fd04164c
deepthi.bhagi@gmail.com <deepthi.bhagi@gmail.com@b05dabfe-00af-a497-5e9e-33324a6027fb>
2013-04-04 10:37:46 +0000

    Done By : B S Deepthi

---
+++ trunk/Ebay2013/src/ebay/action/ShowItemDetails.java
@@ -0,0 +1,73 @@
+package ebay.action;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import models.Item;
+import models.Status;
+import models.Trans;
+import models.User;
+
+import com.opensymphony.xwork2.ActionContext;
+
+public class ShowItemDetails {
+	
+	String userID="";
+	String username="";
+	private int order;
+	private User sellerName;
+	private static ArrayList<Trans> orderItemList = new ArrayList<Trans>();
+	
+	
+	public ArrayList<Trans> getOrderItemList() {
+		return orderItemList;
+	}
+	public void setOrderItemList(ArrayList<Trans> orderItemList) {
+		this.orderItemList = orderItemList;
+	}
+	public User getSellerName() {
+		return sellerName;
+	}
+	public void setSellerName(User sellerName) {
+		this.sellerName = sellerName;
+	}
+	public int getOrder() {
+		return order;
+	}
+	public void setOrder(int order) {
+		this.order = order;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	
+	
+	public String execute() throws Exception {
+		
+		User user=new User();
+		Map<String, Object> session = ActionContext.getContext().getSession();
+		user = (User) session.get("user");
+		
+		if(session.get("user") != null && session.get("user") != ""){
+			user = (User) session.get("user");
+			session.put("user", user);
+			userID = user.getUserid();
+			username = user.getUsername();
+		}
+		orderItemList =Trans.fetchOrderDetails(order);
+				
+			return "success";
+}
+
+}
